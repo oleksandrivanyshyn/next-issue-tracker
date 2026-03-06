2bec01a0118f5bcd819698a25e5bb19d05579569
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-06 13:05:18 +0200

    chore: setup prisma

---
+++ src/app/api/issues/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from 'next/server';
+import { z } from 'zod';
+import prisma from '@/lib/prisma';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const parsedBody = createIssueSchema.safeParse(body);
+  if (!parsedBody.success) {
+    return Response.json(parsedBody.error.issues, { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({ data: parsedBody.data });
+  return Response.json(newIssue, { status: 201 });
+}
