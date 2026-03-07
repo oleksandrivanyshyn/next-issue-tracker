1cf3ff51f48325da519a4cf4ee9753cd65bfd5a5
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-07 22:53:22 +0200

    feat: add edit button

--- src/app/issues/[id]/loading.tsx
+++ src/app/issues/[id]/loading.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { Box, Card, Flex, Heading, Skeleton } from '@radix-ui/themes';
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Heading>
+        <Skeleton height="32px" width="80%" />
+      </Heading>
+
+      <Flex gap="3" my="2" align="center">
+        <Skeleton width="60px" height="24px" />
+        <Skeleton width="120px" height="20px" />
+      </Flex>
+
+      <Card className="prose" mt="4">
+        <Flex direction="column" gap="2">
+          <Skeleton height="16px" />
+          <Skeleton height="16px" />
+          <Skeleton height="16px" width="70%" />
+        </Flex>
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
