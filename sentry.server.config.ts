6e97897e95234bee59d16a35e7a34463434a59fc
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-09 18:57:18 +0200

    feat: integrate Sentry for error handling and monitoring across client and server

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://ff98bd03223fa761538e5edf0ced50c5@o4511015492583424.ingest.de.sentry.io/4511015506149456',
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
