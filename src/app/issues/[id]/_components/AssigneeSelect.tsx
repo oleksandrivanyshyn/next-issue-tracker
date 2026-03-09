424d793bad87af26c9c38504b05f30f7ab49a209
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-09 14:46:07 +0200

    refactor: simplify AssigneeSelect logic by extracting useUsers hook and cleaning up assignment handling

--- src/app/issues/[id]/_components/AssigneeSelect.tsx
+++ src/app/issues/[id]/_components/AssigneeSelect.tsx
@@ -9,33 +9,25 @@ const fetchUsers = () =>
   axios.get<User[]>('/api/users').then((res) => res.data);
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    isLoading,
-    error,
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: fetchUsers,
-    staleTime: 60 * 1000,
-  });
+  const { data: users, isLoading, error } = useUsers();
 
   if (isLoading) return <Skeleton height="35px" />;
   if (error) return null;
-
+  const assignIssue = async (userId: string) => {
+    const assignedToUserId = userId === 'unassigned' ? null : userId;
+    await axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId,
+      })
+      .catch(() => {
+        toast.error('Changes could not be saved.');
+      });
+  };
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || 'unassigned'}
-        onValueChange={async (userId) => {
-          const assignedToUserId = userId === 'unassigned' ? null : userId;
-          await axios
-            .patch(`/api/issues/${issue.id}`, {
-              assignedToUserId,
-            })
-            .catch(() => {
-              toast.error('Changes could not be saved.');
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -54,5 +46,10 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
-
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: fetchUsers,
+    staleTime: 60 * 1000,
+  });
 export default AssigneeSelect;
